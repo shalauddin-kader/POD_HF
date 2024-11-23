acd339be6f8f0c88332790a051ff4c9ea5f901a7
Shalauddin Kader <48006907+shalauddin-kader@users.noreply.github.com>
2024-11-23 11:55:25 +0600

    Update generate_image.py

--- generate_image.py
+++ generate_image.py
@@ -9,7 +9,8 @@ def generate_image_huggingface(prompt):
     """
     Generate an image using Hugging Face's Stable Diffusion API with retry logic.
     """
-    api_url = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+    # api_url = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+    api_url = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3"
     headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
 
     payload = {"inputs": prompt}
